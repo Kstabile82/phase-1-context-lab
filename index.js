1576d0006242245bd7ce81de7d937a19fc30852c
KarinaStabile <karina.stabile@gmail.com>
2021-09-25 21:26:29 -0400

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-/* Your Code Here */
+function createEmployeeRecord(emplArray) {
+    const emplObj = { 
+        firstName: emplArray[0], 
+        familyName: emplArray[1], 
+        title: emplArray[2], 
+        payPerHour: emplArray[3], 
+        timeInEvents: [], 
+        timeOutEvents: [], 
+    }
+    return emplObj;     
+} 
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arrOfArrays) {
+    return arrOfArrays.map((employee) => createEmployeeRecord(employee))
+}
+function createTimeInEvent(recordObj, dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+    let newObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),   
+        date
+    }; 
+    recordObj.timeInEvents.push(newObj)
+    return recordObj; 
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeOutEvent(recordObj, dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+    let newObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10), 
+        date
+    }; 
+    recordObj.timeOutEvents.push(newObj);
+    return recordObj; 
+}
 
+function hoursWorkedOnDate(emplRecordObj, date) {
+    const timeIn = emplRecordObj.timeInEvents.find(event => event.date === date).hour
+    const timeOut = emplRecordObj.timeOutEvents.find(event => event.date === date).hour
+    return (timeOut-timeIn)/100; 
+    
+}
+function wagesEarnedOnDate(emplRecordObj, date) {
+        return emplRecordObj.payPerHour * hoursWorkedOnDate(emplRecordObj, date);
+    
+}
+
+function calculatePayroll(emplRecordsArr) {
+    const calcPay = emplRecordsArr.map((employee) => { 
+        return employee.timeInEvents.map((event) => wagesEarnedOnDate(employee, event.date))
+    })
+    //rewrite calcpayroll using allwagesfor 
+    
+    return calcPay.map((employee) => {
+       let totalVar = 0; 
+       return totalVar += employee.reduce((total, amount) => total + amount)
+    }).reduce((amount, total) => amount + total)
+
+}
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
